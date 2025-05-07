03441b61b676d141f2d757d02e17a8ddb073b684
Yatika Jena <j.yatika@iitg.ac.in>
2025-05-07 23:49:00 +0530

    recursion basics

---
+++ recursion/fibonacci.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int fib(int n){
+    if(n<=0){
+        return 0;
+    }
+    if(n==1){
+        return 1;
+    }
+    return (fib(n-2)+fib(n-1));
+}
+int main() {
+    int n=6;
+    cout<<fib(n);
+
+}
