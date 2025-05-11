bd13fae70b89ae8a341a35540bac8e3e8107a869
Yatika Jena <j.yatika@iitg.ac.in>
2025-05-11 17:34:56 +0530

    dp needs intuition ;)

---
+++ dp/subset-sum-problem.cpp
@@ -0,0 +1,66 @@
+//{ Driver Code Starts
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    bool isSubsetSum(vector<int>& arr, int sum) {
+        vector<vector<bool>> dp(arr.size()+1,vector<bool>(sum+1,false));
+        
+            for(int i=0;i<arr.size()+1;i++){
+                
+                    dp[i][0]=true;
+                
+            }
+        
+        for(int i=1;i<arr.size()+1;i++){
+            for(int j=0;j<=sum;++j){
+                if(arr[i-1]>j){
+                    dp[i][j]=dp[i-1][j];
+                }
+                else{
+                    dp[i][j]=dp[i-1][j] || dp[i-1][j-arr[i-1]];
+                }
+            }
+        }
+        return dp[arr.size()][sum];
+        
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        int sum;
+        cin >> sum;
+        cin.ignore();
+
+        Solution ob;
+        if (ob.isSubsetSum(arr, sum))
+            cout << "true" << endl;
+        else
+            cout << "false" << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
