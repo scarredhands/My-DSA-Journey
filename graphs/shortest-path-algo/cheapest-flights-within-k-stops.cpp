a5b40151b2db430d452421802f6ddbb2c0206574
Yatika Jena <152592973+scarredhands@users.noreply.github.com>
2025-05-29 01:01:05 +0530

    Update and rename heapest-flights-within-k-stops.cpp to cheapest-flights-within-k-stops.cpp

--- graphs/shortest-path-algo/cheapest-flights-within-k-stops.cpp
+++ graphs/shortest-path-algo/cheapest-flights-within-k-stops.cpp
@@ -2,24 +2,26 @@
 
 //mindblowing problem
 // you have to give priority to stops make pq of type {stop,dis,node}
+// have to use queue instead of pq cuz the stops are already sorted.
+//edges= flights.size();
 
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> pq;
-        pq.push({0,0,src});
+        queue<vector<int>> q;
+        q.push({0,0,src});
         vector<int> dis(n,INT_MAX);
         dis[src]=0;
         vector<pair<int,int>> adj[n];
         for(auto it:flights){
             adj[it[0]].push_back({it[1],it[2]});
         }
-        while(!pq.empty()){
-            auto it=pq.top();
+        while(!q.empty()){
+            auto it= q.front();
             int d=it[1];
             int stop=it[0];
             int node=it[2];
-            pq.pop();
+            q.pop();
             // if(node==dst){
             //     return d;
             // }
@@ -28,7 +30,7 @@ public:
                 int node2=it.first;
                 int w=it.second;
                 if(w+d<dis[node2]){
-                     pq.push({stop+1,d+w,node2});
+                     q.push({stop+1,d+w,node2});
                      dis[node2]=d+w;
                 }
                    
