0672440323b19c0cb04187e1c24c3453e5ce8166
Yatika Jena <152592973+scarredhands@users.noreply.github.com>
2025-06-11 11:53:30 +0530

    Create Check it two trees are Identical or Not .cpp

---
+++ trees/Check it two trees are Identical or Not .cpp
@@ -0,0 +1,28 @@
+//https://leetcode.com/problems/same-tree/submissions/1660509506/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSame(TreeNode* p, TreeNode* q){
+        if(p==nullptr || q==nullptr){
+            return p==q;
+        }
+        return p->val==q->val && isSame(p->left,q->left) && isSame(p->right,q->right);
+    }
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if(p==nullptr || q==nullptr){
+            return p==q;
+        }
+        return p->val==q->val && isSame(p->left,q->left) && isSame(p->right,q->right);
+    }
+};
