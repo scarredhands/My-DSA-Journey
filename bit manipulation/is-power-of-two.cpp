c9164ace4f619791732bb66ec69373c15735d906
Yatika Jena <j.yatika@iitg.ac.in>
2025-06-19 00:05:30 +0530

    started bit manipulation

---
+++ bit manipulation/is-power-of-two.cpp
@@ -0,0 +1,14 @@
+//https://leetcode.com/problems/power-of-two/
+
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if(n<=0) return false;
+        if((n&(n-1))==0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+};
