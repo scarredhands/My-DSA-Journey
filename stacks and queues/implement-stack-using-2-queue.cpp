55eb7318406361534d5795f1fac9c18af2645c48
Yatika Jena <j.yatika@iitg.ac.in>
2025-06-21 02:58:30 +0530

    I will freaking do it

---
+++ stacks and queues/implement-stack-using-2-queue.cpp
@@ -0,0 +1,54 @@
+//https://leetcode.com/problems/implement-stack-using-queues/
+
+class MyStack {
+private:
+    queue<int> q1,q2;
+public:
+
+    MyStack() {
+        
+
+    }
+    
+    void push(int x) {
+        q1.push(x);
+    }
+    
+    int pop() {
+        while(q1.size()>1){
+            q2.push(q1.front());
+            q1.pop();
+        }
+        int val=q1.front();
+        q1.pop();
+        swap(q1,q2);
+        return val;
+
+    }
+    
+    int top() {
+        while(q1.size()>1){
+            q2.push(q1.front());
+            q1.pop();
+        }
+        int val=q1.front();
+
+        q2.push(val);
+        q1.pop();       
+         swap(q1,q2);
+        return val;
+    }
+    
+    bool empty() {
+        return q1.empty();
+    }
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
