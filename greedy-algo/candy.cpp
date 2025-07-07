369896202de9a0d4db3727430e3341b482258d09
Yatika Jena <152592973+scarredhands@users.noreply.github.com>
2025-07-08 01:22:51 +0530

    Create candy.cpp

---
+++ greedy-algo/candy.cpp
@@ -0,0 +1,30 @@
+//https://leetcode.com/problems/candy/
+
+//very imp
+
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int n=ratings.size();
+        vector<int> candies(n,1);
+        int ans=0;
+
+        for(int i=1;i<n;++i){
+            if(ratings[i]>ratings[i-1] && candies[i]<=candies[i-1]){
+                
+                candies[i]+=(candies[i-1]-candies[i]+1);
+            }
+        }
+        for(int i=n-2;i>=0;i--){
+            if(ratings[i]>ratings[i+1] && candies[i]<=candies[i+1]){
+             
+                candies[i]+=(candies[i+1]-candies[i]+1);
+            }
+        }
+
+        for(auto it:candies){
+            ans+=it;
+        }
+        return ans;
+    }
+};
