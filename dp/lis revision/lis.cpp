d568f750a6f9a56a94e2a759d305fe92f5e62c39
Yatika Jena <j.yatika@iitg.ac.in>
2025-07-14 20:48:25 +0530

    Create lis.cpp

---
+++ dp/lis revision/lis.cpp
@@ -0,0 +1,25 @@
+//https://leetcode.com/problems/longest-increasing-subsequence/
+//tc= O(N);
+
+//pls revise
+
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n=nums.size();
+        int cnt=1;
+        vector<int> temp;
+        temp.push_back(nums[0]);
+        for(int i=1;i<n;++i){
+            if(nums[i]>temp.back()){
+                temp.push_back(nums[i]);
+                cnt++;
+            }
+            else{
+                auto ind=lower_bound(temp.begin(),temp.end(),nums[i]);
+                *ind=nums[i];
+            }
+        }
+        return cnt;
+    }
+};
