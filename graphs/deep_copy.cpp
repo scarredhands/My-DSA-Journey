2720ce31b692a4ec1ffd7b728ce88482d4c48fe3
Yatika Jena <152592973+scarredhands@users.noreply.github.com>
2025-07-20 20:00:33 +0530

    Create deep_copy.cpp

---
+++ graphs/deep_copy.cpp
@@ -0,0 +1,45 @@
+//https://neetcode.io/problems/clone-graph?list=blind75
+
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+private:
+   //param node is original node here
+   Node* dfs(Node* node,map<Node*,Node*>& oldToNew){
+    if(!node) return nullptr;
+    if(oldToNew.count(node)){
+        return oldToNew[node];
+    }
+    Node* copy= new Node(node->val);
+    oldToNew[node]=copy;
+    for(auto it: node->neighbors){
+        copy->neighbors.push_back(dfs(it,oldToNew));
+    }
+    return copy;
+   }
+public:
+    Node* cloneGraph(Node* node) {
+        //original node, corresponding copy node
+        map<Node*,Node*> oldToNew;
+        return dfs(node,oldToNew);
+    }
+};
