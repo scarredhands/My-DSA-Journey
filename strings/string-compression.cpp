d78ea0e1905773aa1826f02ba3a2856958456f49
Yatika Jena <152592973+scarredhands@users.noreply.github.com>
2025-07-22 13:05:07 +0530

    Create string-compression.cpp

---
+++ strings/string-compression.cpp
@@ -0,0 +1,26 @@
+//https://leetcode.com/problems/string-compression/
+//tc= O(N)
+//its like 3 pointers problem
+
+class Solution {
+public:
+    int compress(vector<char>& chars) {
+        int curr=0; //for modifying the array in place
+        
+        int i=0; // start ptr of the repeating char
+       while(i<chars.size()){
+        int len=1; //end ptr for the repeating char
+         while(i+len<chars.size() && chars[i+len]==chars[i]){
+            len++;
+        }
+        chars[curr++]=chars[i];
+        if(len>1){
+            for(auto ch: to_string(len)){
+                chars[curr++]=ch;
+            }
+        }
+        i+=len;
+       }
+       return curr; //new size of the array
+    }
+};
