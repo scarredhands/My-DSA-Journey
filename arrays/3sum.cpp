aed0531b4e884b1e7fee60f272a4f6afabbe4d1a
Yatika Jena <152592973+scarredhands@users.noreply.github.com>
2025-07-27 15:48:09 +0530

    Create 3sum.cpp

---
+++ arrays/3sum.cpp
@@ -0,0 +1,33 @@
+//https://neetcode.io/problems/three-integer-sum?list=blind75
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        vector<vector<int>> ans;
+        for(int i=0;i<nums.size()-1;++i){
+            if(i>0 && nums[i]==nums[i-1]) continue;
+            int l=i+1;
+          
+            int r=nums.size()-1;
+            
+            while(l<r){
+                int sum=nums[i]+nums[l]+nums[r];
+                if(sum==0){
+                    ans.push_back({nums[i],nums[l],nums[r]});
+                    l++;
+                    r--;
+                    while(r>=0 && nums[r]==nums[r+1]) r--;
+                    while(l<nums.size() && nums[l]==nums[l-1]) l++;
+                }
+                else if(sum<0){
+                    l++;
+                }
+                else{
+                    r--;
+                }
+            }
+        }
+        return ans;
+    }
+};
